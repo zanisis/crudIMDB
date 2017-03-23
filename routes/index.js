072be295078a4a0c046268e0dda42eabeaef0832
zanisis <adzannyaa@gmail.com>
2017-03-23 15:57:01 +0700

    CRUD done-fitur belum

--- routes/index.js
+++ routes/index.js
@@ -5,39 +5,23 @@ var db = require('../models');
 var express = require('express')
 var app = express()
 
-// app.get('/change', function (req, res) {
-//   res.send('Hello World!')
-// })
-//
-// app.listen(2800)
-
-
 /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   db.Movie.findById(1)
-//               .then(function (_movies){
-//                 _movies.getGenres()
-//                         .then(function(_genres){
-//                             //console.log(genre.genre)
-//                             res.render('index', { title: 'Expresso', movies:_movies, genres : _genres });
-//                         })
-//                         //cacth error blm ngerti
-//               })
-//
-// });
+
 router.get('/', function(req, res, next) {
-  db.Movie.findAll()
+  db.Movie.findAll({models: {order:'id ASC'}})
           .then(function (_movies){
             res.render('index', { movies:_movies});//cacth error blm ngerti
-              })
+          })
 });
 
 router.get('/admin', function(req, res, next) {
   db.Movie.findAll()
-          .then(function (_movies){
-            res.render('admin', { tittle: 'Admin Territory', movies:_movies});//cacth error blm ngerti
-              })
-// res.render('admin', {tittle: 'Admin Territory'})
+    .then(function (_movies){
+      res.render('admin', { tittle: 'Admin Territory', movies:_movies});//cacth error blm ngerti
+    })
+    .catch((err)=>{
+      res.send(err.message)
+    })
 });
 
 router.get('/create', function(req, res, next) {
@@ -62,5 +46,32 @@ router.get('/delete/:id',function (req, res, next) {
   })
 })
 
+router.get('/update/:id',function (req, res, next) {
+  db.Movie.findById(req.params.id)
+          .then(function (_movie){
+            res.render('update',
+              {
+                tittle: 'Page Update',
+                movie : _movie
+              }
+            );
+          })
+})
+
+router.post('/update/:id',function (req, res, next) {
+  // res.send('test')
+  let params = req.params.id
+  db.Movie
+    .update({
+      judul: req.body.tittle,
+      isi:  req.body.desc
+    }, {where: {id: params}})
+    .then(()=>{
+      res.redirect('/admin')
+    })
+    .catch((err)=>{
+      res.send(err.message)
+    })
+})
 
 module.exports = router;
